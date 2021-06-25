d89fdc62e225b5ddbf4066163d63c0eed7c57e8c
Renan Tiberto <79670787+renantiberto@users.noreply.github.com>
2021-06-25 18:00:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,109 @@
+// FUNCIONALIDADE DO MENU, ABERTURA E FECHAMENTO, CLIQUE
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// OCULTAR MENU AO CLICAR NO ITEM DE MENU
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// AÇÃO DE SCROLL NA PÁGINA, MUDAR O HEADER
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+// CAROUSEL COM SWIPER (SLIDER) - TESTIMONIALS
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+// SCROLL REVEAL: MOSTRAR ELEMENTOS AO ROLAR A PÁGINA
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+// BOTÃO PARA VOLTAR PARA O TOPO
+const backToTopButtom = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButtom.classList.add('show')
+  } else {
+    backToTopButtom.classList.remove('show')
+  }
+}
+
+// MENU ATIVO DE ACORDO COM A SEÇÃO ATIVA NA PÁGINA
+const sections = document.querySelectorAll('section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+// WHEN SCROLL
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
